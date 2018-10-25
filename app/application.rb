c9732068d4f2bb2d2a630412a60ef95ae9dc2511
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-25 16:31:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,6 +19,7 @@ class Application
         resp.write "Your cart is empty."
       else
         @@cart.each { |item| resp.write "#{item}\n" }
+      end
     else
       resp.write "Path Not Found"
     end
