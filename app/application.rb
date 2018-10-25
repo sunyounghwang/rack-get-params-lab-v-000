c291cf8d960febc3a8902cd7b08b6a8f04175e30
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-25 16:42:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -41,7 +41,7 @@ class Application
   def handle_adding_item(item)
     if @@items.include?(item)
       @@cart << item
-      "added #{item}"
+      "We added #{item} to your cart"
     else
       "We don't have that item"
     end
