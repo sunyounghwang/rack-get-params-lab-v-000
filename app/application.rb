b8a3d6d3a08afcac83a322f143ac605dcdc68025
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-25 16:43:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,10 +11,11 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      
+
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty."
