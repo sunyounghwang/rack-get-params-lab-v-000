4e5dd6bee9cfc50ded679b2a5d86c6b93a1b8769
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-25 16:32:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,7 +21,7 @@ class Application
         @@cart.each { |item| resp.write "#{item}\n" }
       end
     elsif req.path.match(/add/)
-      
+      item = req.params["item"]
     else
       resp.write "Path Not Found"
     end
