cb3732cd7249d8541b71979bdf49903723d8b0dd
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-25 16:44:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,11 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.empty? ? resp.write "Your cart is empty." : @@cart.each { |item| resp.write "#{item}\n" }
+      if @@cart.empty?
+        resp.write "Your cart is empty."
+      else
+        @@cart.each { |item| resp.write "#{item}\n" }
+      end
     elsif req.path.match(/add/)
       item = req.params["item"]
       resp.write handle_adding_item(item)
