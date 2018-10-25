595c9aeaa0983b133d1c66ea84a0986cde4338df
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-25 16:44:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,18 +15,12 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-
     elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty."
-      else
-        @@cart.each { |item| resp.write "#{item}\n" }
+      @@cart.empty? ? resp.write "Your cart is empty." : @@cart.each { |item| resp.write "#{item}\n" }
       end
-
     elsif req.path.match(/add/)
       item = req.params["item"]
       resp.write handle_adding_item(item)
-      
     else
       resp.write "Path Not Found"
     end
