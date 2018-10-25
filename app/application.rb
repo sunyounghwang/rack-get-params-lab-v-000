383d064fc839f6a985f2444571b8e45aa2b273b6
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-25 16:29:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif reg.path.match(/cart/)
+    elsif req.path.match(/cart/)
       resp.write "Your cart is empty." if @@cart.empty?
     else
       resp.write "Path Not Found"
