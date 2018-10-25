d228ed0c2ecaa4f434317ab635e271d88b0400a3
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-25 16:45:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,6 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
